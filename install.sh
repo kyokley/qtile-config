ba2e58b6011dfebe803854ed6d0edddc22492bed
Kevin Yokley <kyokley2@gmail.com>
2018-05-21 18:44:44 -0500

    Adding VT and GCal envs to install script

--- install.sh
+++ install.sh
@@ -15,4 +15,11 @@ $HOME/.pyenv/versions/qtile/bin/pip install -r requirements.txt
 
 mkdir -p ~/.config/qtile
 ln -s "$(pwd)/config.py" ~/.config/qtile/config.py
+ln -s "$(pwd)/autostart.sh" ~/.config/qtile/autostart.sh
 sudo ln -s $(pwd)/qtile.desktop /usr/share/xsessions/
+
+pyenv virtualenv 3.6.2 vt_env
+$HOME/.pyenv/versions/vt_env/bin/pip install git+https://github.com/kyokley/vittlify-cli.git
+
+pyenv virtualenv 3.6.2 gcal_env
+$HOME/.pyenv/versions/gcal_env/bin/pip install git+https://github.com/kyokley/gcalcli.git
