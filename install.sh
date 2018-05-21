c56552543979a5ad74768e064c07d9ec0b5cc5cf
Kevin Yokley <kyokley2@gmail.com>
2018-05-21 14:52:19 -0500

    Installing pip deps in pyenv environment

--- install.sh
+++ install.sh
@@ -5,13 +5,14 @@ set -e
 sudo aptitude install libxcb-render0-dev libffi-dev libcairo2-dev libpangocairo-1.0-0
 
 pyenv virtualenv 3.6.2 qtile
-pyenv activate qtile
 
-pip install xcffib
-pip install cairocffi
-pip install qtile
-pip install -r requirements.txt
+$HOME/.pyenv/versions/qtile/bin/pip install six
+$HOME/.pyenv/versions/qtile/bin/pip install cffi
+$HOME/.pyenv/versions/qtile/bin/pip install xcffib
+$HOME/.pyenv/versions/qtile/bin/pip install cairocffi
+$HOME/.pyenv/versions/qtile/bin/pip install qtile
+$HOME/.pyenv/versions/qtile/bin/pip install -r requirements.txt
 
 mkdir -p ~/.config/qtile
 ln -s "$(pwd)/config.py" ~/.config/qtile/config.py
-sudo cp $(pwd)/qtile.desktop /usr/share/xsessions/
+sudo ln -s $(pwd)/qtile.desktop /usr/share/xsessions/
