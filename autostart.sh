0371b87e5057226f4c024fd0f5ebbb209baf21a0
Kevin Yokley <kyokley2@gmail.com>
2018-06-09 09:08:45 -0500

    Ignoring xautolock in upper right corner

--- autostart.sh
+++ autostart.sh
@@ -2,4 +2,4 @@
 
 nitrogen --restore &
 nm-applet &
-xautolock -locker "$HOME/.pyenv/versions/qtile/bin/python $HOME/.config/qtile/pylocker.py" -time 10 -notify 10 -notifier "notify-send -t 5000 -i gtk-dialog-info 'Locking in 10 seconds'" &
+xautolock -locker "$HOME/.pyenv/versions/qtile/bin/python $HOME/.config/qtile/pylocker.py" -time 10 -notify 10 -notifier "notify-send -t 5000 -i gtk-dialog-info 'Locking in 10 seconds'" -corners 0-00 &
