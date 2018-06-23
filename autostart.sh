9108b6096f5bb8488f8259ea0151cd260de64f51
Kevin Yokley <kyokley2@gmail.com>
2018-06-23 11:22:13 -0500

    Adding 10 min dpms monitor shutoff to autostart script

--- autostart.sh
+++ autostart.sh
@@ -3,3 +3,4 @@
 nitrogen --restore &
 nm-applet &
 xautolock -locker "$HOME/.pyenv/versions/qtile/bin/python $HOME/.config/qtile/pylocker.py" -time 10 -notify 10 -notifier "notify-send -t 5000 -i gtk-dialog-info 'Locking in 10 seconds'" -corners 0-00 &
+xset dpms 600 600 600
