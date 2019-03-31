daabecdeb5b9a88413a32a6f705245e5214d0a9f
Kevin Yokley <kyokley2@gmail.com>
2019-03-31 08:13:30 -0500

    Remove apt installs

--- install.sh
+++ install.sh
@@ -6,14 +6,11 @@ PY3=3.6.7
 
 docker ps
 
-sudo aptitude install libxcb-render0-dev \
-                      libffi-dev \
-                      libcairo2-dev \
-                      libpangocairo-1.0-0 \
-                      thunar \
-                      dmenu \
-                      nitrogen \
-                      xautolock
+pamac install dmenu-manjaro \
+              thunar-volman \
+              nitrogen \
+              xcb-util-xrm \
+              xautolock
 
 xdg-mime default Thunar.desktop inode/directory
 
@@ -35,29 +32,7 @@ sudo ln -s $(pwd)/qtile.desktop /usr/share/xsessions/
 docker pull kyokley/vt
 
 # Install i3lock-color
-cur_dir="$(pwd)"
-git clone https://github.com/PandorasFox/i3lock-color.git /tmp/i3lock-color
-cd /tmp/i3lock-color
-git checkout $(git describe --tags --abbrev=0)
-sudo aptitude install -y libev-dev \
-                         libxcb-composite0 \
-                         libxcb-composite0-dev \
-                         libxcb-xinerama0 \
-                         libxcb-xinerama0-dev \
-                         libxcb-xkb-dev \
-                         libxcb-image0-dev \
-                         libxcb-util-dev \
-                         libxkbcommon-x11-dev \
-                         libjpeg-turbo8-dev \
-                         libpam0g-dev \
-                         libxcb-randr0 \
-                         libxcb-randr0-dev \
-                         libxkbcommon-dev \
-                         libxcb-xrm-dev \
-                         autoconf
-autoreconf -i && ./configure && make && sudo make install
-cd "$cur_dir"
-rm -rf /tmp/i3lock-color
+pamac build i3lock-color
 xset dpms 600 600 600
 
 virtualenv -p python3 /tmp/gcal_env
