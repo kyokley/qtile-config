f6d65310fe427000e49b9f732f5bd53aacfb2954
Kevin Yokley <kyokley2@gmail.com>
2019-04-09 10:08:23 -0500

    Apply new wallpapers as part of external script

--- activate_external.sh
+++ activate_external.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 ps ax | grep intel-virtual-output | grep -v grep >/dev/null && echo "Running" || sudo intel-virtual-output && echo "Started intel-virtual-output" && sleep 1
 
 echo "Activating fake resolution"
@@ -18,4 +16,8 @@ sleep 5
 
 echo "Restarting qtile"
 /home/yokley/.pyenv/versions/qtile/bin/qtile-cmd -o cmd -f restart > /dev/null 2>&1
+
+echo "Applying new background wallpapers"
+bash /home/yokley/workspace/switch-background/switch.sh
+
 echo "Done"
