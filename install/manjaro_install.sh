67f0f79030cc3bedf9c54d618d193337aee838d6
Kevin Yokley <kyokley2@gmail.com>
2019-11-13 18:57:16 -0600

    Move install files to install dir

---
+++ install/manjaro_install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+
+PY3=3.6.7
+
+docker ps
+
+pamac install rofi \
+              thunar-volman \
+              nitrogen \
+              xcb-util-xrm \
+              xautolock \
+              kitty \
+              compton
+
+cp -r ./rofi ~/.config/rofi
+
+xdg-mime default Thunar.desktop inode/directory
+
+pyenv virtualenv $PY3 qtile
+
+$HOME/.pyenv/versions/qtile/bin/pip install --upgrade pip \
+                                            cffi \
+                                            xcffib \
+                                            cairocffi
+
+$HOME/.pyenv/versions/qtile/bin/pip install six \
+                                            qtile \
+                                            -r requirements.txt
+
+if [ ! -h "~/.config/qtile" ]
+then
+    ln -s "$(pwd)" ~/.config/qtile
+fi
+
+if [ ! -h "~/.config/compton" ]
+then
+    ln -s "$(pwd)/compton" ~/.config/compton
+fi
+
+if [ ! -h "/usr/share/xsessions/qtile.desktop" ]
+then
+    sudo ln -s $(pwd)/qtile.desktop /usr/share/xsessions/
+fi
+
+docker pull kyokley/vt
+
+# Install i3lock-color
+pamac build i3lock-color
+xset dpms 600 600 600
+
+virtualenv -p python3 /tmp/gcal_env
+/tmp/gcal_env/bin/pip install git+https://github.com/kyokley/gcalcli.git
+/tmp/gcal_env/bin/gcalcli agenda
+rm -rf /tmp/gcal_env
+
+docker pull kyokley/gcalcli
