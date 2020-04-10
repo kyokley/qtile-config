2782692aaee76690f6d5c9aadfa01aa9e4e1d950
Kevin Yokley <kyokley2@gmail.com>
2020-04-10 08:06:54 -0500

    Comment out xset screensaver disabling

--- autostart.sh
+++ autostart.sh
@@ -4,3 +4,6 @@ nitrogen --restore &
 nm-applet &
 xautolock -locker "$HOME/.config/qtile/force_lock.sh" -time 10 -notify 10 -notifier "notify-send -t 5000 -i gtk-dialog-info 'Locking in 10 seconds'" &
 xset dpms 600 600 600
+
+# Disable screensaver
+# xset s off
