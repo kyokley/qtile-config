20c67d78343b64d59fd5316f96cca340ea3afdcc
Kevin Yokley <kyokley2@gmail.com>
2020-07-29 10:00:51 -0500

    Fix up manjaro install

--- install/manjaro_install.sh
+++ install/manjaro_install.sh
@@ -20,7 +20,7 @@ cp -r ./rofi ~/.config/rofi
 
 xdg-mime default Thunar.desktop inode/directory
 
-pyenv virtualenv-delete $PY3 qtile
+pyenv virtualenv-delete -f qtile | true
 pyenv virtualenv $PY3 qtile
 
 $HOME/.pyenv/versions/qtile/bin/pip install --upgrade pip \
@@ -30,11 +30,13 @@ $HOME/.pyenv/versions/qtile/bin/pip install --upgrade pip \
 
 $HOME/.pyenv/versions/qtile/bin/pip install -r requirements.txt
 
+rm -r ~/.config/qtile
 ln -ns "$(pwd)" ~/.config/qtile
 
 # Picom is a fork of compton but aliases itself to compton for
 # backwards compatibility. Update all references to picom once
 # all distros have been updated.
+rm -r ~/.config/compton
 ln -ns "$(pwd)/compton" ~/.config/compton
 
 if [ ! -h "/usr/share/xsessions/qtile.desktop" ]
@@ -45,7 +47,7 @@ fi
 docker pull kyokley/vt
 
 # Install i3lock-color
-pamac build i3lock-color
+pamac install i3lock-color
 xset dpms 600 600 600
 
 python3 -m venv /tmp/gcal_env
