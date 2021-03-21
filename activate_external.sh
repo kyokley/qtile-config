37b165cdd489cdbb513badc89b1a935cacacadb5
Kevin Yokley <kyokley2@gmail.com>
2021-03-21 14:18:11 -0500

    Upgrade qtile call to 0.17 form

--- activate_external.sh
+++ activate_external.sh
@@ -4,48 +4,48 @@ LEFT_OR_RIGHT='left'
 
 function restart_qtile() {
     echo "Restarting qtile"
-    /home/yokley/.pyenv/versions/qtile/bin/qtile-cmd -o cmd -f restart > /dev/null 2>&1
+    DISPLAY=:0 /home/yokley/.pyenv/versions/qtile/bin/qtile cmd-obj -o cmd -f restart
     echo "qtile restarted successfully"
 }
 
-(ps ax | grep intel-virtual-output | grep -v grep >/dev/null && echo "intel-virtual-output already running" ) || (intel-virtual-output && echo "Started intel-virtual-output" && sleep 1)
+(ps ax | grep intel-virtual-output | grep -v grep >/dev/null && echo "intel-virtual-output already running" ) || (DISPLAY=:0 intel-virtual-output && echo "Started intel-virtual-output" && sleep 1)
 
-VIRTUAL_DISPLAY=$(xrandr -q | grep -Po 'VIRTUAL\d(?= connected)')
+VIRTUAL_DISPLAY=$(DISPLAY=:0 xrandr -q | grep -Po 'VIRTUAL\d(?= connected)')
 
 if [ $? -ne 0 ]
 then
     echo 'Could not find external display'
     restart_qtile
-    xset +dpms
+    DISPLAY=:0 xset +dpms
     exit
 fi
 
 echo "VIRTUAL_DISPLAY: " $VIRTUAL_DISPLAY
 
-PRIMARY_DISPLAY=$(xrandr -q | grep primary | awk '{print $1}')
+PRIMARY_DISPLAY=$(DISPLAY=:0 xrandr -q | grep primary | awk '{print $1}')
 echo "PRIMARY_DISPLAY: " $PRIMARY_DISPLAY
 
-VIRTUAL_MODE=$(xrandr -q | grep $VIRTUAL_DISPLAY | awk 'NR == 2 {print $1}')
+VIRTUAL_MODE=$(DISPLAY=:0 xrandr -q | grep $VIRTUAL_DISPLAY | awk 'NR == 2 {print $1}')
 echo "VIRTUAL_MODE: " $VIRTUAL_MODE
 
-# VIRTUAL_RATE=$(xrandr -q | grep $VIRTUAL_DISPLAY | awk 'NR == 2 {print $NF}' | grep -Po '[0-9.]+')
+# VIRTUAL_RATE=$(DISPLAY=:0 xrandr -q | grep $VIRTUAL_DISPLAY | awk 'NR == 2 {print $NF}' | grep -Po '[0-9.]+')
 # echo "VIRTUAL_RATE: " $VIRTUAL_RATE
 
 echo
 
 echo "Activating fake resolution"
-xrandr --output $VIRTUAL_DISPLAY --${LEFT_OR_RIGHT}-of $PRIMARY_DISPLAY --mode 1680x1050
+DISPLAY=:0 xrandr --output $VIRTUAL_DISPLAY --${LEFT_OR_RIGHT}-of $PRIMARY_DISPLAY --mode 1680x1050
 
 echo "Sleeping for 5 secs..."
 sleep 5
 
 echo "Activating external with resolution $VIRTUAL_MODE"
-xrandr --output $VIRTUAL_DISPLAY --${LEFT_OR_RIGHT}-of $PRIMARY_DISPLAY --mode $VIRTUAL_MODE
+DISPLAY=:0 xrandr --output $VIRTUAL_DISPLAY --${LEFT_OR_RIGHT}-of $PRIMARY_DISPLAY --mode $VIRTUAL_MODE
 
 echo "Sleeping for 5 secs..."
 sleep 5
 
 restart_qtile
-xset -dpms
+DISPLAY=:0 xset -dpms
 
 echo "Done"
