dff19394cdc88f74a2d906d1b39bd508c35bdb98
Kevin Yokley <kyokley2@gmail.com>
2021-06-27 19:08:44 -0500

    Use xautolock -exit

--- toggle_autolock.sh
+++ toggle_autolock.sh
@@ -5,8 +5,7 @@ RETURN_CODE=$?
 
 if [ $RETURN_CODE -eq 0 ] && [ "$PID" != "" ]
 then
-    echo "PID: $PID RETURN_CODE: $RETURN_CODE"
-    kill $PID
+    xautolock -exit
 else
     xautolock -locker "$HOME/.config/qtile/force_lock.sh" -time 10 -notify 10 -notifier "notify-send -t 5000 -i gtk-dialog-info 'Locking in 10 seconds'" &
 fi
